8dfefda7b78537222060d2029fe6e13a13d0ac6f
bamboguirassy <didegassama@gmail.com>
2020-05-22 15:58:24 +0000

    removing mes reclamations function service

--- src/app/pages/bourse/reclamation_bourse/reclamation_bourse.service.ts
+++ src/app/pages/bourse/reclamation_bourse/reclamation_bourse.service.ts
@@ -20,10 +20,6 @@ export class ReclamationBourseService {
     return this.httpSrv.get(this.getRoutePrefixWithSlash() + id);
   }
 
-  findMesReclamations() {
-    return this.httpSrv.get(this.getRoutePrefixWithSlash() + 'mes-reclamations/');
-  }
-
   create(reclamationBourse: ReclamationBourse) {
     return this.httpSrv.post(this.getRoutePrefixWithSlash() + 'create', reclamationBourse);
   }
