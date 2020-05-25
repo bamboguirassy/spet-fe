371abaf510786aa8ec6bfe4438dd66a55f246abd
Babacar Faye <faye.babacar78@gmail.com>
2020-05-25 20:28:20 +0000

    https://trello.com/c/fl5nAfJ6

--- src/app/pages/mon-parcours/mon-parcours.component.ts
+++ src/app/pages/mon-parcours/mon-parcours.component.ts
@@ -1,26 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { Inscriptionacad } from '../inscriptionacad/inscriptionacad';
-import { InscriptionacadService } from '../inscriptionacad/inscriptionacad.service';
-import { ActivatedRoute } from '@angular/router';
-import { Preinscription } from '../preinscription/preinscription';
+import {Component, OnInit} from '@angular/core';
+import {Inscriptionacad} from '../inscriptionacad/inscriptionacad';
+import {InscriptionacadService} from '../inscriptionacad/inscriptionacad.service';
+import {ActivatedRoute} from '@angular/router';
+import {Preinscription} from '../preinscription/preinscription';
 
 @Component({
-  selector: 'app-mon-parcours',
-  templateUrl: './mon-parcours.component.html',
-  styleUrls: ['./mon-parcours.component.scss']
+    selector: 'app-mon-parcours',
+    templateUrl: './mon-parcours.component.html',
+    styleUrls: ['./mon-parcours.component.scss']
 })
 export class MonParcoursComponent implements OnInit {
-  public type = 'grid';
-  public searchText: string;
-  inscriptions: Inscriptionacad[] = [];
-  preinscriptions: Preinscription[] = [];
-  constructor(public inscriptionAcadSrv: InscriptionacadService,
-              public activatedRoute: ActivatedRoute) { 
-              }
+    public type = 'grid';
+    public searchText: string;
+    inscriptions: Inscriptionacad[] = [];
+    preinscriptions: Preinscription[] = [];
+    display = false;
 
-  ngOnInit() {
-    this.inscriptions = this.activatedRoute.snapshot.data.inscriptions;
-    this.preinscriptions = this.activatedRoute.snapshot.data.preinscriptions;
-  }
+
+    constructor(
+        public inscriptionAcadSrv: InscriptionacadService,
+        public activatedRoute: ActivatedRoute) {
+    }
+
+    ngOnInit() {
+        this.inscriptions = this.activatedRoute.snapshot.data.inscriptions;
+        this.preinscriptions = this.activatedRoute.snapshot.data.preinscriptions;
+    }
+
+    showDialog() {
+        this.display = true;
+    }
+
+    sendAdministratifDocRequest(inscriptionacad: Inscriptionacad) {
+        console.log(inscriptionacad);
+        // set state => default is 1
+        // set acad year
+    }
 
 }
