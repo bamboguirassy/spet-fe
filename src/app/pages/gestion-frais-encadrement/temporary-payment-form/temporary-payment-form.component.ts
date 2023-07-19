ce55577e045d9c00b85bd10b9960618ae11e3964
thiate0 <sthiate.thioune@univ-thies.sn>
2023-07-19 10:47:08 +0000

    Interfaces details

--- src/app/pages/gestion-frais-encadrement/temporary-payment-form/temporary-payment-form.component.ts
+++ src/app/pages/gestion-frais-encadrement/temporary-payment-form/temporary-payment-form.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+
 
 interface City {
     name: number;
     code: number;
 }
-
-
 
 @Component({
     selector: 'app-temporary-payment-form',
@@ -14,28 +12,14 @@ interface City {
     styleUrls: ['./temporary-payment-form.component.scss']
 })
 export class TemporaryPaymentFormComponent implements OnInit {
-
     cities: City[] | undefined;
 
-    formGroup: FormGroup | undefined;
-
+    selectedCity: City | undefined;
     ngOnInit() {
         this.cities = [
             { name: 1, code: 1 },
             { name: 2, code: 2 },
-            { name: 3, code: 3},
+            { name: 3, code: 3 },
         ];
-
-        this.formGroup = new FormGroup({
-            // selectedCity: new FormControl<City | null>(null)
-            selectedCity: new FormControl<City | null>(null)
-        });
     }
-
 }
-
-
-
-
-
-
